618f542bda072f26e4100dede36013b11d8f7525
kiel019 <202080497@psu.palawan.edu.ph>
2023-12-30 00:47:46 +0800

    Optimize movie search

--- scripts.js
+++ scripts.js
@@ -38,47 +38,25 @@ const upcomingMovies = async() =>{
 
 upcomingMovies()
 
-function searchMovies() {
+const searchMovies = async() => {
     const query = document.getElementById('search').value;
 
-    // Make sure the user entered a query
     if (query.trim() === '') {
         alert('Please enter a movie title.');
         return;
     }
 
-    // Construct the URL for the TMDb API request
-    const url = `${baseUrl}?api_key=${apiKey}&query=${encodeURIComponent(query)}`;
-
-    // Make the API request using Axios
-    axios.get(url)
-        .then(response => displayResults(response.data.results))
-        .catch(error => console.error('Error fetching data:', error));
-}
-
-function displayResults(results) {
-    nextButton.style.display = 'none';
-    prevButton.style.display = 'none';
-    resultsContainer.innerHTML = '';
-
-    if (results.length === 0) {
-        resultsContainer.innerHTML = '<p>No results found.</p>';
-        return;
-    }
-
-    results.forEach(movie => {
-        window.scrollTo(0,0);
-        const listItem = document.createElement('li');
-        listItem.classList.add('movie');
-        // Check if the movie has a poster path
-        const posterPath = movie.poster_path;
-        const posterUrl = posterPath
-            ? `https://image.tmdb.org/t/p/w500${posterPath}`
-            : 'placeholder_image_url.jpg';  // Provide a placeholder image URL if no poster is available
-        
-            listItem.innerHTML = `<div ondblclick="showMovieDetails(${movie.id})"><strong>${movie.title}</strong> (${movie.release_date})<br><img class=posterImg src="${posterUrl}" alt="${movie.title} Poster"></div>`;
-        resultsContainer.appendChild(listItem);
-    });
+    const response = await fetch(`${baseUrl}?api_key=${apiKey}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    resultsContainer.innerHTML = `
+        <strong class='um'>Search Results</strong>
+        <div class='searchMovies'>
+        ${data.results.slice(0, 15).map((data)=> (
+        `<div class='uMovies' ondblclick="showMovieDetails(${data.id})">
+        <img class=posterImg src="http://image.tmdb.org/t/p/w500/${data.poster_path}" alt="${data.title} Poster">
+        <strong>${data.title}</strong> (${data.release_date})
+        </div>`)).join('')}
+        </div>`
 }
 
 function showMovieDetails(movieId) {
